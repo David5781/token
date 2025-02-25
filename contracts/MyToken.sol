607753e43d5e0b9defe47d418ae6a8cd9568e010
David <david.traverser@gmail.com>
2025-02-25 14:41:40 +0800

    合约.sol代码及开源.json文件提交

--- contracts/MyToken.sol
+++ contracts/MyToken.sol
@@ -2,12 +2,10 @@
 pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
 
-contract FirstToken is ERC20, ERC20Permit {
-    constructor() ERC20("FirstToken", "ft") ERC20Permit("FirstToken") {
-        // 在部署时将 10 亿个代币 mint 给合约部署者
-        uint256 totalSupply = 1_000_000_000 * (10 ** uint256(decimals())); // 10 亿个代币
+contract MEVGO is ERC20 {
+    constructor() ERC20("MEVGO", "MG") {
+        uint256 totalSupply = 1_000_000_000 * (10 ** uint256(decimals())); 
         _mint(msg.sender, totalSupply);
     }
 }
